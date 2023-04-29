6c240b150137c36bac38e34fafed0896449b9290
bikbaevartur <bigbyar4i@gmail.com>
2023-04-29 15:32:46 +0300

    hw_3_3

--- hw_3.py
+++ hw_3.py
@@ -20,6 +20,7 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+
 import random
 
 
@@ -28,43 +29,67 @@ def new_array(size):
     return array
 
 
-def serch_num(array, num):
+def search_num(array, num):
     count = 0
-    nearest_num = array[1]
-    min_diff = num - array[1]
-    temp_diff = 0
+
     for i in range(len(array)):
         if array[i] == num:
             count += 1
-        else:
-            if 
 
-
-
-            
-            
-            
-            
-        
     return count
 
 
+def nearest_num(array, num):
+    arr_length = len(array)
+    nearest_num = array[0]
+    diff = abs(num - nearest_num)
+    for i in range(1, arr_length):
+        curr_diff = abs(num - array[i])
+        if curr_diff < diff:
+            diff, nearest_num = curr_diff, array[i]
+
+    return nearest_num
 
 
 size = int(input("Input size list: "))
-num_serch = int(input("Input num: "))
+num_search = int(input("Input num: "))
 
 new_list = new_array(size)
 print(new_list)
-result = serch_num(new_list, num_serch)
+result = search_num(new_list, num_search)
 
 if result > 0:
-    print(f"число {num_serch} встречается {result} раз")
+    print(f"число {num_search} встречается {result} раз")
+else:
+    nearest = nearest_num(new_list, num_search)
+    print(f"числа {num_search} нету в списке, ближайшее число {nearest}")
 
 
-# *Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность. В случае с английским алфавитом очки распределяются так:A, E, I, O, U, L, N, S, T, R – 1 очко; D, G – 2 очка; B, C, M, P – 3 очка; F, H, V, W, Y – 4 очка; K – 5 очков; J, X – 8 очков; Q, Z – 10 очков. А русские буквы оцениваются так: А, В, Е, И, Н, О, Р, С, Т – 1 очко; Д, К, Л, М, П, У – 2 очка; Б, Г, Ё, Ь, Я – 3 очка; Й, Ы – 4 очка; Ж, З, Х, Ц, Ч – 5 очков; Ш, Э, Ю – 8 очков; Ф, Щ, Ъ – 10 очков. Напишите программу, которая вычисляет стоимость введенного пользователем слова. Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
+# *Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+#  В случае с английским алфавитом очки распределяются так:
+# A, E, I, O, U, L, N, S, T, R – 1 очко;
+#  D, G – 2 очка; B, C, M, P – 3 очка;
+# F, H, V, W, Y – 4 очка; K – 5 очков;
+# J, X – 8 очков;
+# Q, Z – 10 очков.
+
+
+# А русские буквы оцениваются так:
+#  А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+#  Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка;
+#  Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков;
+# Ш, Э, Ю – 8 очков;
+# Ф, Щ, Ъ – 10 очков.
+
+
+#  Напишите программу, которая вычисляет стоимость введенного пользователем слова.
+# Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
 
 # *Пример:*
 
 # ноутбук
 #     12
+
+scrabble_list = [{ "1" :'A' 'E' 'I' 'O' 'U' 'L' 'N' 'S' 'T' 'R' 'А' 'В' 'Е' 'И' 'Н' 'О' 'Р' 'С' 'Т'}]
