4c23fa6e1d907ca82556a9b3838b9e78c5aed5e8
ArturBikbaev <bigbyar4i@gmail.com>
2023-05-11 22:18:34 +0300

    hw_5

---
+++ ln_3/ln_3.py
@@ -0,0 +1,31 @@
+# def sumnumbers(n):
+#     summ =0
+#     for i in range(1, n+1):
+#         summ += i
+#     print(summ)
+
+# sumnumbers(5) 
+
+# def sum_str(*args):  #(*args) неограниченное кол во аргументов
+#     res = ''
+#     for i in args:
+#         res += i
+#     return res
+
+# print(sum_str('q', 'e', 'l'))
+
+# import modul1
+
+# print(modul1.max1(5,9))
+
+# from modul1 import max1  тот же  вариант, но не надо писать modul1
+
+# print(max1(9,10))
+
+#from modul1 import * b
+
+# import modul1 as m1  изменяем название modul1 на время применение на m1
+
+def fib(n):
+    if n
+ 
