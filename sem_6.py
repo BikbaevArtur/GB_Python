809ec2c192e2de121e0b38b955b5dbac815853a0
bikbaevartur <bigbyar4i@gmail.com>
2023-05-28 22:42:41 +0300

    sem_6_

--- sem_6.py
+++ sem_6.py
@@ -35,3 +35,49 @@
 # вводится число N — количество элементов в массиве
 # Далее записаны N чисел — элементы массива. Массив
 # состоит из целых чисел.
+# import random
+# list_one = [random.randint(1,10) for _ in range(10) ]
+# print(list_one)
+# count = 0
+# for i in range(1,len(list_one)-1):
+#     if list_one[i-1] < list_one[i] and list_one[i+1] < list_one[i]:
+#         count += 1
+# print(count)
+
+# result = [1 for i in range(1,len(list_one)-1) if list_one[i-1]< list_one[i]>list_one[i+1]]
+# print(len(result))
+
+# Задача №43. Решение в группах
+# Дан список чисел. Посчитайте, сколько в нем пар
+# элементов, равных друг другу. Считается, что любые
+# два элемента, равные друг другу образуют одну пару,
+# которую необходимо посчитать. Вводится список
+# чисел. Все числа списка находятся на разных
+# строках
+
+def list_array(min_val,max_val,size):
+    import random
+    new_list = [random.randint(min_val,max_val) for _ in range(size)]
+    return new_list
+
+# def double(min_val,max_val,list_serch):
+#     while(min_val < max_val):
+#         count =0
+#         count_1 = 0
+#         for i in range(len(list_serch)):
+#             if min_val == list_serch[i]:
+#                 count +=1
+#         print(f'{min_val} : {count}')
+#         min_val +=1
+        
+def uniq_dic(list_1):
+    uniq_dict = {}
+    for num in list_1:
+        uniq_dict[num] = uniq_dict.get(num,0)+1
+    print(uniq_dict)
+    return uniq_dict
+
+
+list_1 = list_array(1,10,10)
+print(list_1)
+uniq_dic(list_1)
