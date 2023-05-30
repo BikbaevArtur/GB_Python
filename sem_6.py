2847abc0f334680ff94388eb25d221af46d67924
ArturBikbaev <bigbyar4i@gmail.com>
2023-05-30 22:54:18 +0300

    sem_5 2:11

--- sem_6.py
+++ sem_6.py
@@ -24,7 +24,7 @@
 #     if item not in list_2:
 #         list_3.append(item)
 # print(list_3)
-#можно так
+# можно так
 # list_3 = [i for i in list_1 if i not in list_2]
 
 # Задача №41. Решение в группах
@@ -55,9 +55,9 @@
 # чисел. Все числа списка находятся на разных
 # строках
 
-def list_array(min_val,max_val,size):
+def list_array(min_val, max_val, size):
     import random
-    new_list = [random.randint(min_val,max_val) for _ in range(size)]
+    new_list = [random.randint(min_val, max_val) for _ in range(size)]
     return new_list
 
 # def double(min_val,max_val,list_serch):
@@ -69,15 +69,76 @@ def list_array(min_val,max_val,size):
 #                 count +=1
 #         print(f'{min_val} : {count}')
 #         min_val +=1
-        
-def uniq_dic(list_1):
-    uniq_dict = {}
-    for num in list_1:
-        uniq_dict[num] = uniq_dict.get(num,0)+1
-    print(uniq_dict)
-    return uniq_dict
+
+# def uniq_dic(list_1):
+#     uniq_dict = {}
+#     for num in list_1:
+#         uniq_dict[num] = uniq_dict.get(num,0)+1
+#     return uniq_dict
+
+# def double_num(dict_1):
+#     count = 0
+#     for (kay,val) in dict_1.items():
+#         if val >= 2 :
+#             while val > 1:
+#                 count +=1
+#                 val= val // 2
+#     return count
+
+# list_1 = list_array(1,10,10)
+# print(list_1)
+# dic_un = uniq_dic(list_1)
+# print(dic_un)
+# double_n = double_num(dic_un)
+# print(double_n)
 
 
-list_1 = list_array(1,10,10)
+# import random  #решение
+# my_list = [random.randint(1,10) for _ in range(30)]
+
+# repeats = sum([my_list.count(i) // 2 for i in set(my_list)])
+# print (my_list)
+# print(repeats)
+
+# Задача №45. Решение в группах
+# Два различных натуральных числа n и m называются
+# дружественными, если сумма делителей числа n
+# (включая 1, но исключая само n) равна числу m и
+# наоборот. Например, 220 и 284 – дружественные числа.
+# По данному числу k выведите все пары дружественных
+# чисел, каждое из которых не превосходит k. Программа
+# получает на вход одно натуральное число k, не
+# превосходящее 105
+# . Программа должна вывести все
+# пары дружественных чисел, каждое из которых не
+# превосходит k. Пары необходимо выводить по одной в
+# строке, разделяя пробелами. Каждая пара должна быть
+# выведена только один раз (перестановка чисел новую
+# пару не дает).
+
+k = int(input())
+list_1 = []
+
+# def new_list(num):
+#     my_list1 = []
+#     for i in range(1, num):
+#         if num % i == 0:
+#             my_list1.append(i)
+#     return my_list1
+
+
+def sum_list(number):
+    sum = 0
+    for i in range(1,number//2 +1):
+        if number % i ==0:
+            sum += i
+    return sum
+
+while(k < 300):
+    a1 = sum_list(k)
+    a2 = sum_list(a1)
+    if a1 == k and a2 == a1:
+        list_1.append(k)
+        list_1.append(a1)
+
 print(list_1)
-uniq_dic(list_1)
