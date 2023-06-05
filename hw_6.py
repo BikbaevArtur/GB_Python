3220a5caa501bf56979f70bd017d00750cf6ed0b
bikbaevartur <bigbyar4i@gmail.com>
2023-06-05 20:15:25 +0300

    hw_8

--- hw_6.py
+++ hw_6.py
@@ -7,15 +7,15 @@
 
 # Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
 
-def new_list(size,min_val,max_val):
-    import random
-    return [random.randint(min_val,max_val) for _ in range(size)]
+# def new_list(size,min_val,max_val):
+#     import random
+#     return [random.randint(min_val,max_val) for _ in range(size)]
 
 
-def serch_index(list_serch,n,n1):
-        for i in range(len(list_serch)):
-            if n <= list_serch[i] <= n1 :
-                print(f'{list_serch[i]} индекс {i}')
+# def serch_index(list_serch,n,n1):
+#         for i in range(len(list_serch)):
+#             if n <= list_serch[i] <= n1 :
+#                 print(f'{list_serch[i]} индекс {i}')
         
 
 
@@ -24,6 +24,6 @@ def serch_index(list_serch,n,n1):
 
 
 
-list_1 = new_list(10,1,10)
-result = serch_index(list_1,2,6)
-print(result)
+# list_1 = new_list(10,1,10)
+# result = serch_index(list_1,2,6)
+# print(result)
